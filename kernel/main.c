b7918c1e7124fb7c831341f3be28bf2358f97a04
xzf <xzf2112578094@163.com>
2026-06-23 22:28:26 +0800

    fix():增加CSR和SP寄存器读取函数

--- kernel/main.c
+++ kernel/main.c
@@ -2,6 +2,26 @@
 #include "uart.h"
 #include "print.h"
 
+/**
+ * @brief 证书读取函数，读取CSR寄存器
+ */
+static inline uint64_t read_mhartid(void)
+{
+    uint64_t value;
+    __asm__ volatile ("csrr %0, mhartid" : "=r"(value));   /**< 汇编指令模板，第一个冒号后是输出字段，从硬件读数据给C变量而不是把C变量传进去 */
+    return value;
+}
+
+/**
+ * @brief 读取 X2通用寄存器
+ */
+static inline uint64_t read_sp(void)
+{
+    uint64_t value;
+    __asm__ volatile ("mv %0, sp" : "=r"(value));         /**< 将value值映射到一个通用寄存器，该寄存器作为mv指令的目标操作数 */
+    return value;
+}
+
 void kmain(void){
     uart_puts("Hello from RISC-V kernel!\n");
 
@@ -11,6 +31,14 @@ void kmain(void){
 
     uart_puts("Test dec: ");
     uart_putdec(2026);                          /**< 验证取余逆序算法正确性 */
+    uart_puts("\n");
+
+    uart_puts("Hart id: ");
+    uart_putdec(read_mhartid());
+    uart_puts("\n");
+
+    uart_puts("sp: ");
+    uart_puthex(read_sp());
     uart_puts("\n");
 
     uart_puts("Kernel boot success.\n");
